b88d3c888ac578f975b835c039278aa7e68d603b
rabah babaci <4rabah@gmail.com>
2020-02-06 18:19:26 -0800

    import styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import PicOfTheDay from "./components/PicOfTheDay";
+import styled from "styled-components";
 
 function App() {
   return (
