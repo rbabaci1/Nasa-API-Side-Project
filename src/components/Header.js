058199ce18165fbeb82a0b937633dc044fe880aa
rabah babaci <4rabah@gmail.com>
2020-02-06 19:02:00 -0800

    add style to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,31 @@
 import React from "react";
+import styled from "styled-components";
 
-const Header = ({ date }) => {
+const Header = () => {
+  const Intro = styled.div`
+    width: 80%;
+    margin: 20px auto;
+    border-bottom: 3px solid #8a8584;
+
+    h1 {
+      font-family: Monaco;
+      color: #595969;
+    }
+    p {
+      font-size: 18px;
+      font-family: monospace;
+      line-height: 25px;
+      color: #be5e0a;
+
+      a {
+        text-decoration: none;
+        font-weight: bold;
+      }
+    }
+  `;
+
   return (
-    <div>
+    <Intro>
       <h1>Astronomy Picture of the Day!</h1>
       <p>
         <a href="https://apod.nasa.gov/apod/archivepix.html" target="blank">
@@ -13,7 +36,7 @@ const Header = ({ date }) => {
         astronomer.
       </p>
       <p>{"2020 February 6"}</p>
-    </div>
+    </Intro>
   );
 };
 
