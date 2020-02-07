2456bdb41b16fc37efb740197142759796ec79dc
rabah babaci <4rabah@gmail.com>
2020-02-06 17:49:22 -0800

    set the nasa data to the returned data form the API

--- src/components/PicOfTheDay.js
+++ src/components/PicOfTheDay.js
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const PicOfTheDay = () => {
   const [nasaData, setNasaData] = useState({});
+
+  function fetchAPI() {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=F8dSoRxUrpecxL46FCau6nMt5PUmynrEb5ox9uUr&date=2020-02-06"
+      )
+      .then(response => setNasaData(response.data))
+      .catch(error => console.error(error));
+  }
+
+  useEffect(fetchAPI, []);
+  console.log(nasaData);
 
   return (
     <div>
