ed842f967290710fdd1a1ac377eb742937018a2b
rabah babaci <4rabah@gmail.com>
2020-02-06 17:10:18 -0800

    add content to the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const Header = () => {
-  return <h1>Hello there</h1>;
+const Header = ({ date }) => {
+  return (
+    <div>
+      <h1>Astronomy Picture of the Day!</h1>
+      <p>
+        <a href="https://apod.nasa.gov/apod/archivepix.html" target="blank">
+          Discover the cosmos!
+        </a>{" "}
+        Each day a different image or photograph of our fascinating universe is
+        featured, along with a brief explanation written by a professional
+        astronomer.
+      </p>
+      <p>{"2020 February 6"}</p>
+    </div>
+  );
 };
 
 export default Header;
