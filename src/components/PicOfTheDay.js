3ae34b96c1181495cdf0151d04c8de8742c37096
rabah babaci <4rabah@gmail.com>
2020-02-06 17:27:32 -0800

    import and test axios

--- src/components/PicOfTheDay.js
+++ src/components/PicOfTheDay.js
@@ -1,7 +1,16 @@
 import React from "react";
+import axios from "axios";
 
 const PicOfTheDay = ({ date }) => {
-  return null;
+  axios
+    .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-02-06")
+    .then(r => console.log(r))
+    .catch(error => console.error(error));
+  return (
+    <div>
+      <h2>yess</h2>
+    </div>
+  );
 };
 
 export default PicOfTheDay;
