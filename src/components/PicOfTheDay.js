108c4e5ecc1ef8cb9f6e17149daa51d32771c968
rabah babaci <4rabah@gmail.com>
2020-02-06 17:34:30 -0800

    initialize the nasa state data

--- src/components/PicOfTheDay.js
+++ src/components/PicOfTheDay.js
@@ -1,11 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 const PicOfTheDay = () => {
-  axios
-    .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-02-06")
-    .then(r => console.log(r))
-    .catch(error => console.error(error));
+  const [nasaData, setNasaData] = useState({});
+
   return (
     <div>
       <h2>yess</h2>
